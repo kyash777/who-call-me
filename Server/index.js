929c5a72a748e7063dee54ff6931705d6f92fb32
YashPlayto <yash@playto.so>
2024-08-09 14:44:40 +0530

    modified cors

--- Server/index.js
+++ Server/index.js
@@ -12,7 +12,10 @@ dotenv.config()
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+}));
+
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }))
 
