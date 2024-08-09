96852f0e4fd94822c9be94f36adb0e9c94d21faa
YashPlayto <yash@playto.so>
2024-08-09 15:06:31 +0530

    added headers

--- Server/index.js
+++ Server/index.js
@@ -16,6 +16,13 @@ app.use(cors({
     origin: ['https://who-call-me-dild.vercel.app'],
 }));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://who-call-me-dild.vercel.app'); // Update with your client URL
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
 
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }))
